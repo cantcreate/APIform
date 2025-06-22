1fa63a6a1063e5e104e1569ead2c99f685dba0e7
cantcreate <adhikariprashamsa586@gmail.com>
2025-06-22 13:36:57 +0545

    feat: increase fetched items from 1 to 30

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 document.getElementById("btn").addEventListener("click", fetchData);
+
 async function fetchData() {
   try {
     const response = await fetch("https://dummyjson.com/products");
     const data = await response.json();
-    const product = data.products[0];
-    const newrow = `<tr>
-    <td>${product.id}</td>
-    <td>${product.title}</td>
-    <td>${product.price}</td>
-    </tr>
-    `;
-    document.querySelector("#tablue tbody").innerHTML = newrow;
 
-    document.getElementById("id").value = product.id;
-    document.getElementById("productname").value = product.title;
-    document.getElementById("price").value = product.price;
+    const tbody = document.querySelector("#tablue tbody");
+
+    data.products.forEach((product) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${product.id}</td>
+        <td>${product.title}</td>
+        <td>${product.price}</td>
+      `;
+      tbody.appendChild(row);
+    });
+
+    const productname = data.products[0];
+    document.getElementById("id").value = productname.id;
+    document.getElementById("productname").value = productname.title;
+    document.getElementById("price").value = productname.price;
   } catch (error) {
-    document.querySelector("#tablue tbody").innerHTML = `${error.message}`;
+    document.querySelector("#tablue tbody").innerHTML = `
+     ${error.message}
+    `;
   }
 }
