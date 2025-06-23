ebfbee1b53c5b2533cee2a33da2c2567eca54923
cantcreate <adhikariprashamsa586@gmail.com>
2025-06-23 16:21:42 +0545

    Implement localStorage for saving and loading form data

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+window.onload = firstload;
+
 function myfunction() {
   const entry = {
     id: document.getElementById("id").value,
     producttitle: document.getElementById("productname").value,
     price: document.getElementById("price").value,
   };
+
+  const entries = JSON.parse(localStorage.getItem("formentries")) || [];
+  entries.push(entry);
+  localStorage.setItem("formentries", JSON.stringify(entries));
+
   addToTable(entry);
+
   document.getElementById("form").reset();
+}
+function firstload() {
+  const saved = JSON.parse(localStorage.getItem("formentries")) || [];
+  saved.forEach((entry) => addToTable(entry));
 }
 function addToTable(entry) {
   const table = document
